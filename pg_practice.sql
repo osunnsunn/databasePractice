81c623c3460351aac09cad73cd72fedd97c28796
Shun Fukuda <osunnrnjoy@gmail.com>
2025-10-03 15:57:17 +0900

    pg_practice.sqlの修正

--- pg_practice.sql
+++ pg_practice.sql
@@ -2,15 +2,19 @@
 CREATE DATABASE name;
 
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
-CREATE TABLE users
-(id char(4) not null,
-name text not null,
- food text not null,
-PRIMARY KEY(id));
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL DEFAULT '',
+    age INTEGER NOT NULL DEFAULT 0,
+    gender TEXT NOT NULL DEFAULT 'Other' CHECK (gender IN ('Man', 'Woman', 'Other'))
+);
+
 
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
-CREATE TABLE jods
-(id char(4) not null,
-name text not null,
- food text not null,
-PRIMARY KEY(id));
+CREATE TABLE jobs (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    name VARCHAR(255) NOT NULL DEFAULT '会社員',
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+);
+COMMENT ON COLUMN jobs.name IS '仕事名';
