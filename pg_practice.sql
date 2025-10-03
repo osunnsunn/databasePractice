c8a1f143ce8041a885432f8caa801bd1a3bbf49b
Shun Fukuda <osunnrnjoy@gmail.com>
2025-10-03 14:07:06 +0900

    DDL課題の提出

--- pg_practice.sql
+++ pg_practice.sql
@@ -1,5 +1,16 @@
 -- 問題1. postgresqlでpracticeデータベースを生成するSQL文を記述してください。
+CREATE DATABASE name;
 
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
+CREATE TABLE users
+(id char(4) not null,
+name text not null,
+ food text not null,
+PRIMARY KEY(id));
 
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
+CREATE TABLE jods
+(id char(4) not null,
+name text not null,
+ food text not null,
+PRIMARY KEY(id));
